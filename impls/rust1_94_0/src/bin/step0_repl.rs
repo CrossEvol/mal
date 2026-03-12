c898e21111467c1c191dd204d18197b133abdd96
CrossEvol <296662402@qq.com>
2026-03-12 20:44:52 +0800

    rust1_94_0: step0_repl

--- impls/rust1_94_0/src/bin/step0_repl.rs
+++ impls/rust1_94_0/src/bin/step0_repl.rs
@@ -1,3 +1,25 @@
+use rust1_94_0::readline as rl;
+
+fn read(input: &str) -> &str {
+    input
+}
+
+fn eval(input: &str) -> &str {
+    input
+}
+
+fn print(input: &str) -> &str {
+    input
+}
+
+fn rep(input: &str) -> &str {
+    print(eval(read(input)))
+}
+
 fn main() {
-    println!("Hello, step0_rep!");
+    loop {
+        let input = rl::readline("user> ").unwrap();
+        let result = rep(&input);
+        println!("{}", result);
+    }
 }
