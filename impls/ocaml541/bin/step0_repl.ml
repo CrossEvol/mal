eddadc111a9680e62d31e6ccc2489843b31ea4ad
CrossEvol <296662402@qq.com>
2026-03-25 13:43:25 +0800

    ocaml541: step0_repl

--- impls/ocaml541/bin/step0_repl.ml
+++ impls/ocaml541/bin/step0_repl.ml
@@ -1,3 +1,12 @@
-open Ocaml541
+open Ocaml541.Rl
 
-let () = print_endline ""
+let () =
+  let rec loop () =
+    let result = readline ~prompt:"user> " () in
+    match result with
+    | Some line ->
+        print_endline line;
+        loop ()
+    | None -> loop ()
+  in
+  loop ()
--- impls/ocaml541/lib/rl.ml
+++ impls/ocaml541/lib/rl.ml
@@ -1,11 +1,14 @@
 let complete _str = Readline.Filenames
+let init_success = ref false
 
 let init_readline () =
   Readline.init ~catch_break:false ~program_name:"readline-example"
     ~history_file:(Sys.getenv "HOME" ^ "/.readline-example_history")
-    ()
+    ();
+  init_success.contents <- true
 
 let readline ?(prompt = "") () =
+  if not !init_success then init_readline ();
   let str = Readline.readline ~completion_fun:complete ~prompt () in
   match str with
   | Some str ->
