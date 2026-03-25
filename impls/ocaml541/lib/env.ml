056002e80b2e50d19a19d9f037ee5ab869037498
CrossEvol <296662402@qq.com>
2026-03-25 17:26:32 +0800

    ocaml541: step3_env

--- impls/ocaml541/lib/env.ml
+++ impls/ocaml541/lib/env.ml
@@ -1,15 +1,23 @@
 open Types
+open Result.Syntax
 
 type env = { outer : env option; data : (string, malVal) Hashtbl.t }
 
 let env_new outer = { outer; data = Hashtbl.create 8 }
 
-let env_set env k v =
+let env_sets env k v =
   if Hashtbl.mem env.data k then Hashtbl.replace env.data k v
   else Hashtbl.add env.data k v
 
+let env_set env k v =
+  match k with
+  | Sym s ->
+      env_sets env s v;
+      Ok v
+  | _ -> error "Env.set called with non-Str"
+
 let rec env_get env k =
-  if Hashtbl.mem k env.data then Hashtbl.find k env.data
+  if Hashtbl.mem env.data k then Some (Hashtbl.find env.data k)
   else match env.outer with Some outer -> env_get outer k | None -> None
 
 let env_bind outer mbinds exprs =
@@ -18,13 +26,13 @@ let env_bind outer mbinds exprs =
       let env = env_new (Some outer) in
       let rec bind_args a b =
         match (a, b) with
-        | [ Sym "&"; Sym name ], args -> env_set env name (list args)
-        | Sym name :: names, arg :: args ->
-            env_set env name arg;
+        | [ Sym "&"; b ], args -> env_set env b (list args)
+        | a :: names, arg :: args ->
+            let* _ = env_set env a arg in
             bind_args names args
-        | [], [] -> ()
+        | [], [] -> Ok Nil
         | _ -> raise (Invalid_argument "Bad param count in fn call")
       in
-      bind_args binds exprs;
+      let* _ = bind_args binds exprs in
       Ok env
   | _ -> error "env_bind binds not List/Vector"
